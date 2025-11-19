7334fc2bafc6dd95847936d4bcfb556fbf4464f3
Shashank3778 <anku3778@gmail.com>
2025-11-19 14:17:52 +0530

    resolve the tab visibility

--- dist/CustomHeader.js
+++ dist/CustomHeader.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { initLucideIcons } from './utilis/iconUtils';
+import { initLucideIcons } from './uitils/iconUtils';
 var CustomHeader = function CustomHeader(_ref) {
   var title = _ref.title,
     toggleSidebar = _ref.toggleSidebar,
--- src/CustomHeader.jsx
+++ src/CustomHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { initLucideIcons } from './utilis/iconUtils';
+import { initLucideIcons } from './uitils/iconUtils';
 
 const CustomHeader = ({ title, toggleSidebar, darkMode, toggleDarkMode, sidebarCollapsed }) => {
   useEffect(() => {
