6be3fd953cff23368edea7534d171377691d77a6
Shashank3778 <anku3778@gmail.com>
2025-11-26 23:24:47 +0530

    make the card css correct

--- src/containers/Dashboard/index.jsx
+++ src/containers/Dashboard/index.jsx
@@ -20,7 +20,7 @@ export const Dashboard = () => {
   const showSelectSessionModal = reduxHooks.useShowSelectSessionModal();
 
   return (
-    <div id="dashboard-container" className="d-flex flex-column p-2 pt-0">
+    <div id="dashboard-container" className="d-flex  p-2 pt-0">
       <h1 className="sr-only">{pageTitle}</h1>
       {!initIsPending && (
         <>
